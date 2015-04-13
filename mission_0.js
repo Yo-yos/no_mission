3672179e622be071c714c79d369f900205330cb4
Yo-yos <yangb07@qq.com>
2015-04-13 20:34:30 +0800

    add max function and tested; add assert function and modified test function

--- mission_0.js
+++ mission_0.js
@@ -18,61 +18,72 @@ function div(a,b)
 	return a/b;
 }
 
-function test_of_add()
+function max(a,b)
 {
-	var sum = add(2,3);
-	if (sum == 5)
+	if (a>b)
 	{
-		console.log('add ok');
+		return a;
 	}
 	else
 	{
-		console.log('add failed');
+		return b;
 	}
+}
+
+function assert(conditon,message)
+{
+	if(conditon)
+	{
+		console.log(message + ' ok');
+	}
+	else
+	{
+		console.log(message + ' failed');
+	}
+}
+
+
+
+function test_of_add()
+{
+	var sum = add(2,3);
+	assert(sum == 5, 'add');
 }
 
 function test_of_sub()
 {
 	var diff = sub(5,2);
-	if (diff == 3)
-	{
-		console.log('sub ok');
-	}
-	else
-	{
-		console.log('sub failed');
-	}
+	assert(diff == 3, 'sub');
 }
 
 function test_of_mul()
 {
-	var pro = mul(2,3);
-	if (pro == 6)
-	{
-		console.log('mul ok');
-	}
-	else
-	{
-		console.log('mul failed');
-	}
+	var prod = mul(2,3);
+	assert(prod == 6,'mul');
 }
 
 function test_of_div()
 {
 	var quo = div(6,2);
-	if (quo == 3)
-	{
-		console.log('div ok');
-	}
-	else
-	{
-		console.log('div failed');
-	}
+	assert(quo == 3, 'div');
+}
+
+function test_of_max1()
+{
+	var m = max(4,2)
+	assert(m == 4, 'given 4 and 2, max 4 is');
+}
+
+function test_of_max2()
+{
+	var m = max(6,10)
+	assert(m == 10, 'given 6 and 10, max 10 is');
 }
 
 test_of_add();
 test_of_sub();
 test_of_mul();
 test_of_div();
-
+test_of_max1();
+test_of_max2();
 
