0a7d7ec3f33b3e3bfe490ab5629ef7d112733893
Yo-yos <yangb07@qq.com>
2015-04-13 09:33:57 +0800

    wrote a readme file

---
+++ mission_0.js
@@ -0,0 +1,76 @@
+function add(a,b)
+{
+	return a+b;
+}
+
+function sub(a,b)
+{
+	return a-b;
+}
+
+function mul(a,b)
+{
+	return a*b;
+}
+
+function div(a,b)
+{
+	return a/b;
+}
+
+function test_of_add()
+{
+	var sum = add(2,3);
+	if (sum == 5)
+	{
+		console.log('add ok');
+	}
+	else
+	{
+		console.log('add failed');
+	}
+}
+
+function test_of_sub()
+{
+	var diff = sub(5,2);
+	if (diff == 3)
+	{
+		console.log('sub ok');
+	}
+	else
+	{
+		console.log('sub failed');
+	}
+}
+
+function test_of_mul()
+{
+	var pro = mul(2,3);
+	if (pro == 6)
+	{
+		console.log('mul ok');
+	}
+	else
+	{
+		console.log('mul failed');
+	}
+}
+
+function test_of_div()
+{
+	var quo = div(6,2);
+	if (quo == 3)
+	{
+		console.log('div ok');
+	}
+	else
+	{
+		console.log('div failed');
+	}
+}
+
+test_of_add();
+test_of_sub();
+test_of_mul();
+test_of_div();
